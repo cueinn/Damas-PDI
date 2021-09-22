08e25576bb1337d6225c1918f3f23d13321a5bc1
Felipe Silva <felipesilva@MacBook-Pro-de-Felipe.local>
2021-09-22 20:28:39 -0300

    Console de peça branca selecionada e estilo de peça selecionada

--- public/script.js
+++ public/script.js
@@ -3,10 +3,15 @@ const tabuleiro = document.getElementById('boardContent');
 // tabuleiro.innerHTML = '<div>OI!</div>'
 
 function cliquePeca(event){
-  if (event.target.classList.contains('dark')) {
-    alert('funcionou!');
+  if (event.target.classList.contains('piece')) {
+    event.target.classList.add('selected');
   }
-  console.log(event.target.classList);
+  if (event.target.classList.contains('dark')) {
+    console.log("clicou em uma peça preta: "+event.target.classList);
+  }
+  if (event.target.classList.contains('light')) {
+    console.log("clicou em uma peça clara "+event.target.classList);
+  }
 }
 
 tabuleiro.addEventListener('click', cliquePeca)
--- public/todolist.md
+++ public/todolist.md
@@ -13,3 +13,10 @@ Renderizar o tabuleiro todo via JS
   Dizer onde vai renderizar
   Montar a marcação no JS
   
+
+-> Clicar nas peças brancas.
+-> Modal abrir.
+-> Modal fechar (com botão jogar).
+-> Modal de instruções (botão de fechar).
+-> Fechar ao clicar fora do modal (tudo que não está dentro do pai, fecha o modal).
+-> Renderizar o tabuleiro via javascript (appendChild, innerHTML).
